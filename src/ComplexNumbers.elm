59db9d062104648839618a3df297ad756f7fd71f
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-02-16 11:35:32 -0500

    Add ComplexNumber Type and add

---
+++ src/ComplexNumbers.elm
@@ -0,0 +1,60 @@
+module ComplexNumbers exposing (ComplexNumber(..))
+
+{-| A module for complex numbers
+For instance, we defined generic `concat` in this module using `Monoid` type as follows.
+
+    concat : Monoid a -> List a -> a
+    concat m =
+        List.foldr (append m) (empty m)
+
+
+# Types
+
+@docs ComplexNumber
+
+
+# Constructors
+
+@docs monoid
+
+
+# Functions for unwraping Monoid
+
+@docs empty
+
+
+# Convenient functions for monoid
+
+@docs concat
+
+
+# Monoid types for popular types
+
+@docs string
+
+-}
+
+-- Types
+
+
+type alias Real =
+    Float
+
+
+type alias Imaginary =
+    Float
+
+
+{-| Main type.
+-}
+type alias ComplexNumber =
+    ( Real, Imaginary )
+
+
+
+-- Complex addition
+
+
+add : ComplexNumber -> ComplexNumber -> ComplexNumber
+add ( realOne, imaginaryOne ) ( realTwo, imaginaryTwo ) =
+    ( realOne + realTwo, imaginaryOne + imaginaryTwo )
