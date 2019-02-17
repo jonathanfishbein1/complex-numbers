6051fae37b637ca6d180bddd4951f669d1858a94
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-02-17 11:04:41 -0500

    Add multiplication distributes over addition test

--- tests/ComplexNumbersTest.elm
+++ tests/ComplexNumbersTest.elm
@@ -106,6 +106,27 @@ suite =
                 in
                 testValueOne
                     |> Expect.equal testValueTwo
+        , Test.fuzz3 (Fuzz.intRange -10 10) (Fuzz.intRange -10 10) (Fuzz.intRange -10 10) "tests ComplexNumbers multiplication distributes over addition" <|
+            \one two three ->
+                let
+                    a =
+                        ComplexNumbers.ComplexNumber (ComplexNumbers.Real one) (ComplexNumbers.Imaginary two)
+
+                    b =
+                        ComplexNumbers.ComplexNumber (ComplexNumbers.Real two) (ComplexNumbers.Imaginary three)
+
+                    c =
+                        ComplexNumbers.ComplexNumber (ComplexNumbers.Real one) (ComplexNumbers.Imaginary three)
+
+                    testValueOne =
+                        ComplexNumbers.multiply a (ComplexNumbers.add b c)
+
+                    testValueTwo =
+                        ComplexNumbers.multiply a b
+                            |> ComplexNumbers.add (ComplexNumbers.multiply a c)
+                in
+                testValueOne
+                    |> Expect.equal testValueTwo
         , Test.fuzz3 Fuzz.float Fuzz.float Fuzz.float "tests ComplexNumbers division" <|
             \one two three ->
                 let
