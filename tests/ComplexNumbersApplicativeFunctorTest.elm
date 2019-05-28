992b2d7289db720e6ff9ab133a050fb963ab3530
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-05-28 19:58:37 -0400

    Add tests for second and fourth applicative functor laws

--- tests/ComplexNumbersApplicativeFunctorTest.elm
+++ tests/ComplexNumbersApplicativeFunctorTest.elm
@@ -79,6 +79,22 @@ suite =
                         ComplexNumbers.applyCartesian pureF pureOne
                 in
                 Expect.equal cApplied expected
+        , Test.fuzz Fuzz.int "tests fourth applicative law for Complex Numbers" <|
+            \one ->
+                let
+                    pureOne =
+                        ComplexNumbers.pureCartesian identity
+
+                    pureTwo =
+                        ComplexNumbers.pureCartesian one
+
+                    leftSide =
+                        ComplexNumbers.applyCartesian pureOne pureTwo
+
+                    rightSide =
+                        ComplexNumbers.applyCartesian (ComplexNumbers.pureCartesian (\_ -> one)) pureOne
+                in
+                Expect.equal leftSide rightSide
         , Test.fuzz2 Fuzz.int Fuzz.int "tests first applicative law for Complex Numbers Polar" <|
             \one two ->
                 let
@@ -148,4 +164,20 @@ suite =
                         ComplexNumbers.applyPolar pureF pureOne
                 in
                 Expect.equal cApplied expected
+        , Test.fuzz Fuzz.int "tests fourth applicative law for Complex Numbers Polar" <|
+            \one ->
+                let
+                    pureOne =
+                        ComplexNumbers.purePolar identity
+
+                    pureTwo =
+                        ComplexNumbers.purePolar one
+
+                    leftSide =
+                        ComplexNumbers.applyPolar pureOne pureTwo
+
+                    rightSide =
+                        ComplexNumbers.applyPolar (ComplexNumbers.purePolar (\_ -> one)) pureOne
+                in
+                Expect.equal leftSide rightSide
         ]
